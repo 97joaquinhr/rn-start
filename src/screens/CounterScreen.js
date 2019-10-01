afff99819121f0ed43f9a1f44253b4d2bd0ed0ad
97joaquinhr <97joaquinhr@gmail.com>
2019-09-30 19:56:23 -0500

    Started counter example

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+const CounterScreen = () => {
+  
+    return (
+        <View>
+            <Text>Current Count:</Text>
+        </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    text: {
+      fontSize: 30
+    }
+  });
+  
+  export default CounterScreen;
+  
