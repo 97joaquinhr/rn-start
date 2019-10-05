c507d60aa35c1eb2e9c4429e3b682d57e1b25d43
97joaquinhr <97joaquinhr@gmail.com>
2019-10-05 09:38:48 -0500

    Started with text demo

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View,Text,StyleSheet} from  'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+const TextScreen= () =>{
+    return (
+        <View>
+            <TextInput style={styles.input}/>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    input:{
+        margin: 15,
+        borderColor: 'black',
+        borderWidth:1
+    }
+});
+export default TextScreen;
